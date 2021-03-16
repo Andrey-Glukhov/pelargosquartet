560ba6b592f2ae1fe341e039486c17bb3e388dc3
Eliza <gluhova.liza@gmail.com>
2021-03-16 11:12:04 +0100

    background color

--- functions.php
+++ functions.php
@@ -34,12 +34,51 @@ function child_theme_menu_logo() {
     $logo_string = '';
     $home_link =  home_url( '/' ) ;
 	if ( $logo != '' && $logo != ' ' ) {
-		
+
 			$logo_string .= '<a href="' . esc_url( $home_link ) . '"><img class="btMainLogo" src="' . esc_attr( $logo ) . '" alt="' . esc_attr( get_bloginfo( 'name' ) ) . '"></a>';
 	} else {
 		$logo_string .= '<a href="' .  esc_url(home_url( '/' )) . '">' .  esc_attr( get_bloginfo( 'name' ) ) . '</a>';
 	}
     return $logo_string;
+}
+
+add_shortcode( 'showhide', 'showhide_shortcode' );
+function showhide_shortcode( $atts, $content = null ) {
+	// Variables
+	$post_id = get_the_id();
+	$word_count = number_format_i18n( sizeof( explode( ' ', strip_tags( $content ) ) ) );
+
+	// Extract ShortCode Attributes
+	$attributes = shortcode_atts( array(
+		'type' => 'pressrelease',
+		'more_text' => __( 'Show Press Release (%s More Words)', 'wp-showhide' ),
+		'less_text' => __( 'Hide Press Release (%s Less Words)', 'wp-showhide' ),
+		'hidden' => 'yes'
+	), $atts );
+
+	// More/Less Text
+	$more_text = sprintf( $attributes['more_text'], $word_count );
+	$less_text = sprintf( $attributes['less_text'], $word_count );
+  //$more_text = '>>>';
+  //$less_text = '<<<';
+	// Determine Whether To Show Or Hide Press Release
+	$hidden_class = 'sh-hide';
+	$hidden_css = 'display: none;';
+	$hidden_aria_expanded = 'false';
+	if( $attributes['hidden'] === 'no' ) {
+		$hidden_class = 'sh-show';
+		$hidden_css = 'display: block;';
+		$hidden_aria_expanded = 'true';
+		$tmp_text = $more_text;
+		$more_text = $less_text;
+		$less_text = $tmp_text;
+	}
+
+	// Format HTML Output
+	$output  = '<div id="' . $attributes['type'] . '-link-show' . $post_id . '" class="sh-link ' . $attributes['type'] . '-link ' . $hidden_class .'"><a href="#" onclick="showhide_show(\'' . esc_js( $attributes['type'] ) . '\', ' . $post_id . '); return false;" aria-expanded="' . $hidden_aria_expanded .'"><span id="' . $attributes['type'] . '-toggle-show' . $post_id . '">' . $more_text . '</span></a></div>';
+	$output .= '<div id="' . $attributes['type'] . '-content-' . $post_id . '" class="sh-content ' . $attributes['type'] . '-content ' . $hidden_class . '" style="' . $hidden_css . '">' . do_shortcode( $content ) ;
+	$output .= '<div id="' . $attributes['type'] . '-link-hide' . $post_id . '" class="sh-link ' . $attributes['type'] . '-link ' . $hidden_class .'"><a href="#" onclick="showhide_hide(\'' . esc_js( $attributes['type'] ) . '\', ' . $post_id . '); return false;" aria-expanded="' . $hidden_aria_expanded .'"><span id="' . $attributes['type'] . '-toggle-hide' . $post_id . '">' . $less_text . '</span></a></div></div>';
+	return $output;
 }
 
 ?>
